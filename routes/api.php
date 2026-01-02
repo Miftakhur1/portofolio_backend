345a447d55d442abf0e509514cb126df0805b1a1
Miftah <miftah.khur.3@gmail.com>
2026-01-02 21:08:42 +0700

    fix: enable api routing in Laravel 11

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\ExperienceController;
 use App\Http\Controllers\Api\SkillControlller;
-// use App\Http\Controllers\Api\ContactMessageController;
+
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/experiences', [ExperienceController::class, 'index']);
-Route::get('/skills', [App\Http\Controllers\Api\SkillControlller::class, 'index']);
-
-
-// Route::GET('/contacts', [ContactMessageController::class, 'store'])
-//     ->middleware('throttle:5,1');
+Route::get('/skills', [SkillControlller::class, 'index']);
